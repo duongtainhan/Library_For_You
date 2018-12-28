7ef51b65a8617848d90f3923ad7d18f127c4e275
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-12-28 23:40:56 +0700

    Auto resize layout

--- app/src/main/java/android/dienty/library_for_you/MainActivity.java
+++ app/src/main/java/android/dienty/library_for_you/MainActivity.java
@@ -8,11 +8,15 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgAnim1;
     ImageView imgAnim2;
+    TextView txtDemo;
+    TextView txtTeam;
+    TextView txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     {
         imgAnim1 = findViewById(R.id.img_anim1);
         imgAnim2 = findViewById(R.id.img_anim2);
+        txtDemo = findViewById(R.id.txtDemo);
+        txtTeam = findViewById(R.id.txtTeam);
+        txtName = findViewById(R.id.txtName);
     }
     private void InitValue() {
 
@@ -38,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void CreateAnimation()
     {
+        //AnimationAlpha ImageView
         AlphaAnimation alphaAnim1 = new AlphaAnimation(0,0.1f);
-        alphaAnim1.setDuration(4000);
+        alphaAnim1.setDuration(2000);
         alphaAnim1.setFillAfter(true);
         alphaAnim1.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -49,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                AlphaAnimation alphaAnim2 = new AlphaAnimation(0,0.3f);
-                alphaAnim2.setDuration(4000);
+                AlphaAnimation alphaAnim2 = new AlphaAnimation(0,0.1f);
+                alphaAnim2.setDuration(2000);
                 alphaAnim2.setFillAfter(true);
                 alphaAnim2.setAnimationListener(new Animation.AnimationListener() {
                     @Override
@@ -77,6 +85,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         imgAnim2.startAnimation(alphaAnim1);
+        //AnimationAlpha TextView
+        AlphaAnimation alphaTextDemo = new AlphaAnimation(0,1);
+        alphaTextDemo.setDuration(1369);
+        alphaTextDemo.setFillAfter(true);
+        alphaTextDemo.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                txtDemo.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                AlphaAnimation alphaTextTeam = new AlphaAnimation(0,1);
+                alphaTextTeam.setDuration(1369);
+                alphaTextTeam.setFillAfter(true);
+                alphaTextTeam.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        txtTeam.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        AlphaAnimation alphaTextName = new AlphaAnimation(0,1);
+                        alphaTextName.setDuration(1369);
+                        alphaTextName.setFillAfter(true);
+                        alphaTextName.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                                txtName.setVisibility(View.VISIBLE);
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+
+                            }
+                        });
+                        txtName.startAnimation(alphaTextName);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                txtTeam.startAnimation(alphaTextTeam);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        txtDemo.startAnimation(alphaTextDemo);
     }
 
 }
