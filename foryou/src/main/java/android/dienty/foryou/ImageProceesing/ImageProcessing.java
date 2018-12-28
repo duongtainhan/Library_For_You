878d043103c3a9417f58fb21a7ae4d7c1c08e48e
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-12-28 14:33:27 +0700

    HighLightImage and InvertImage

---
+++ foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
@@ -0,0 +1,70 @@
+package android.dienty.foryou.ImageProceesing;
+
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+
+public class ImageProcessing {
+    public static Bitmap doHighlightImage(Bitmap src) {
+        // create new bitmap, which will be painted and becomes result image
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth() + 96, src.getHeight() + 96, Bitmap.Config.ARGB_8888);
+        // setup canvas for painting
+        Canvas canvas = new Canvas(bmOut);
+        // setup default color
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+        // create a blur paint for capturing alpha
+        Paint ptBlur = new Paint();
+        ptBlur.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.NORMAL));
+        int[] offsetXY = new int[2];
+        // capture alpha into a bitmap
+        Bitmap bmAlpha = src.extractAlpha(ptBlur, offsetXY);
+        // create a color paint
+        Paint ptAlphaColor = new Paint();
+        ptAlphaColor.setColor(0xFFFFFFFF);
+        // paint color for captured alpha region (bitmap)
+        canvas.drawBitmap(bmAlpha, offsetXY[0], offsetXY[1], ptAlphaColor);
+        // free memory
+        bmAlpha.recycle();
+
+        // paint the image source
+        canvas.drawBitmap(src, 0, 0, null);
+
+        // return out final image
+        return bmOut;
+    }
+    public static Bitmap doInvert(Bitmap src) {
+        // create new bitmap with the same settings as source bitmap
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        // color info
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final bitmap
+        return bmOut;
+    }
+}
