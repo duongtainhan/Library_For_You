3b3ed6bae20368a0e28b4cf115f58e07a6904640
trungnguyenuit2015 <42066015+trungnguyenuit2015@users.noreply.github.com>
2019-01-06 14:17:08 +0700

    Image Processing – Pixel Color Replacement

--- foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
+++ foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
@@ -589,4 +589,66 @@ public static Bitmap flip(Bitmap src, int type) {
     // return transformed image
     return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
 }
+package pete.android.study;
+ 
+import android.graphics.Bitmap;
+ 
+public class ImageProcessor {
+    Bitmap mImage;
+    boolean mIsError = false;
+ 
+    public ImageProcessor(final Bitmap image) {
+        mImage = image.copy(image.getConfig(), image.isMutable());
+        if(mImage == null) {
+            mIsError = true;
+        }
+    }
+ 
+    public boolean isError() {
+        return mIsError;
+    }
+ 
+    public void setImage(final Bitmap image) {
+        mImage = image.copy(image.getConfig(), image.isMutable());
+        if(mImage == null) {
+            mIsError = true;
+        } else {
+            mIsError = false;
+        }
+    }
+ 
+    public Bitmap getImage() {
+        if(mImage == null){
+            return null;
+        }
+        return mImage.copy(mImage.getConfig(), mImage.isMutable());
+    }
+ 
+    public void free() {
+        if(mImage != null && !mImage.isRecycled()) {
+            mImage.recycle();
+            mImage = null;
+        }
+    }
+ 
+    public Bitmap replaceColor(int fromColor, int targetColor) {
+        if(mImage == null) {
+            return null;
+        }
+ 
+        int width = mImage.getWidth();
+        int height = mImage.getHeight();
+        int[] pixels = new int[width * height];
+        mImage.getPixels(pixels, 0, width, 0, 0, width, height);
+ 
+        for(int x = 0; x < pixels.length; ++x) {
+            pixels[x] = (pixels[x] == fromColor) ? targetColor : pixels[x];
+        }
+ 
+        Bitmap newImage = Bitmap.createBitmap(width, height, mImage.getConfig());
+        newImage.setPixels(pixels, 0, width, 0, 0, width, height);
+ 
+        return newImage;
+    }
+}
 }
