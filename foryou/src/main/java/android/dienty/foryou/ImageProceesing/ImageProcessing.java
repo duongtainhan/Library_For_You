0152c42c32ee03f964869f897ec9ecc2327bf1fa
trungnguyenuit2015 <42066015+trungnguyenuit2015@users.noreply.github.com>
2019-01-06 14:11:04 +0700

    Image Processing – Image with Round Corner

--- foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
+++ foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
@@ -516,4 +516,35 @@ public class ImageProcessing {
         }
         return bmOut;
     }
+
+    public static Bitmap roundCorner(Bitmap src, float round) {
+    // image size
+    int width = src.getWidth();
+    int height = src.getHeight();
+    // create bitmap output
+    Bitmap result = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+    // set canvas for painting
+    Canvas canvas = new Canvas(result);
+    canvas.drawARGB(0, 0, 0, 0);
+ 
+    // config paint
+    final Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setColor(Color.BLACK);
+ 
+    // config rectangle for embedding
+    final Rect rect = new Rect(0, 0, width, height);
+    final RectF rectF = new RectF(rect);
+ 
+    // draw rect to canvas
+    canvas.drawRoundRect(rectF, round, round, paint);
+ 
+    // create Xfer mode
+    paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+    // draw source image to canvas
+    canvas.drawBitmap(src, rect, rect, paint);
+ 
+    // return final image
+    return result;
+}
 }
