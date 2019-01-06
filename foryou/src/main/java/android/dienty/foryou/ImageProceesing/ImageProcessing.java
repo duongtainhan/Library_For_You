560e77942c4920db9330d2ae68033796d8b73a09
trungnguyenuit2015 <42066015+trungnguyenuit2015@users.noreply.github.com>
2019-01-06 14:21:28 +0700

    Image Processing – Black Filter (Increasing the Darkness)

--- foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
+++ foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
@@ -737,4 +737,36 @@ public static Bitmap applyFleaEffect(Bitmap source) {
     bmOut.setPixels(pixels, 0, width, 0, 0, width, height);
     return bmOut;
 }
+public static Bitmap applyBlackFilter(Bitmap source) {
+    // get image size
+    int width = source.getWidth();
+    int height = source.getHeight();
+    int[] pixels = new int[width * height];
+    // get pixel array from source
+    source.getPixels(pixels, 0, width, 0, 0, width, height);
+    // random object
+    Random random = new Random();
+ 
+    int R, G, B, index = 0, thresHold = 0;
+    // iteration through pixels
+    for(int y = 0; y < height; ++y) {
+        for(int x = 0; x < width; ++x) {
+            // get current index in 2D-matrix
+            index = y * width + x;
+            // get color
+            R = Color.red(pixels[index]);
+            G = Color.green(pixels[index]);
+            B = Color.blue(pixels[index]);
+            // generate threshold
+            thresHold = random.nextInt(COLOR_MAX);
+            if(R < thresHold && G < thresHold && B < thresHold) {
+                pixels[index] = Color.rgb(COLOR_MIN, COLOR_MIN, COLOR_MIN);
+            }
+        }
+    }
+    // output bitmap
+    Bitmap bmOut = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    bmOut.setPixels(pixels, 0, width, 0, 0, width, height);
+    return bmOut;
+}
 }
