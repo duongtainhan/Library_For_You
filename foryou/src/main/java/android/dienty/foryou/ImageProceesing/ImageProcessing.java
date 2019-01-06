fdc5f7014497a953b2130a86172585b2fdf96c04
trungnguyenuit2015 <42066015+trungnguyenuit2015@users.noreply.github.com>
2019-01-06 14:14:35 +0700

    Update ImageProcessing.java

--- foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
+++ foryou/src/main/java/android/dienty/foryou/ImageProceesing/ImageProcessing.java
@@ -565,4 +565,28 @@ public static Bitmap mark(Bitmap src, String watermark, Point location, Color co
  
     return result;
 }
+// type definition
+public static final int FLIP_VERTICAL = 1;
+public static final int FLIP_HORIZONTAL = 2;
+ 
+public static Bitmap flip(Bitmap src, int type) {
+    // create new matrix for transformation
+    Matrix matrix = new Matrix();
+    // if vertical
+    if(type == FLIP_VERTICAL) {
+        // y = y * -1
+        matrix.preScale(1.0f, -1.0f);
+    }
+    // if horizonal
+    else if(type == FLIP_HORIZONTAL) {
+        // x = x * -1
+        matrix.preScale(-1.0f, 1.0f);
+    // unknown type
+    } else {
+        return null;
+    }
+ 
+    // return transformed image
+    return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+}
 }
